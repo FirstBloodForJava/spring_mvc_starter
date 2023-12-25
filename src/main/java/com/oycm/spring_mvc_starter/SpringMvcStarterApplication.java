eb77ce036c8635aba1da440f084d23988e4f11b5
oycm <1164864987@qq.com>
2023-12-25 21:53:19 +0800

    项目创建

---
+++ src/main/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplication.java
@@ -0,0 +1,12 @@
+package com.oycm.spring_mvc_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcStarterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplicationTests.java
@@ -0,0 +1,7 @@
+package com.oycm.spring_mvc_starter;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringMvcStarterApplicationTests {
+}
