ff0963668433031b976edab0bb735206c2166a69
oycm <1164864987@qq.com>
2023-12-26 21:33:06 +0800

    自定义静态资源访问方式1.代码,2.配置

---
+++ src/main/java/com/oycm/spring_mvc_starter/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.oycm.spring_mvc_starter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 配置文件的配置和这个都会生效
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static/")
+                .setCachePeriod(31556926);
+    }
+}
