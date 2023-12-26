0fb4b017855578aa9c3e21319e5800eaf232b03f
oycm <1164864987@qq.com>
2023-12-26 11:21:55 +0800

    SpringMvc初始化

--- src/main/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplication.java
+++ src/main/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplication.java
@@ -2,9 +2,17 @@ package com.oycm.spring_mvc_starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMvcStarterApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcStarterApplication.class, args);
