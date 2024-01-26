8697d1b18878b3e84d1dd07e6f264a4e1c22b656
oycm <1164864987@qq.com>
2024-01-26 22:13:35 +0800

    减低springboot版本,测试请求体参数接受

--- src/main/java/com/oycm/spring_mvc_starter/controller/ExampleController.java
+++ src/main/java/com/oycm/spring_mvc_starter/controller/ExampleController.java
@@ -1,7 +1,10 @@
 package com.oycm.spring_mvc_starter.controller;
 
+import com.google.gson.Gson;
 import com.oycm.spring_mvc_starter.dto.ReturnInfo;
 import com.oycm.spring_mvc_starter.dto.User;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,8 +14,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ExampleController {
 
+    protected final Log log = LogFactory.getLog(this.getClass());
+
+    private Gson gson = new Gson();
+
     @PostMapping("/static/user")
     public User userInfo(@RequestBody(required = false) User user){
+        log.info(gson.toJson(user));
+        user.setAge(19);
+        return user;
+    }
+
+    @PostMapping("/static/requiredUser")
+    public User requiredUserInfo(@RequestBody User user){
+        log.info(gson.toJson(user));
         user.setAge(19);
         return user;
     }
