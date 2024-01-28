1d2704b680ccbd3b938758b702715491ff9a3be1
oycm <1164864987@qq.com>
2024-01-28 15:03:31 +0800

    注释补充

--- src/main/java/com/oycm/spring_mvc_starter/utils/JacksonUtil.java
+++ src/main/java/com/oycm/spring_mvc_starter/utils/JacksonUtil.java
@@ -21,6 +21,13 @@ public class JacksonUtil {
         return objectMapper.getTypeFactory().constructType(type);
     }
 
+    /**
+     * 字符串转Java对象
+     * @param json json字符串
+     * @param javaType 最终解析的Java对象类Type对应的JavaType
+     * @return 泛型对象
+     * @param <T>
+     */
     public static <T> T stringToObject(String json, JavaType javaType){
         try {
             return objectMapper.readValue(json, javaType);
@@ -29,6 +36,12 @@ public class JacksonUtil {
         }
     }
 
+    /**
+     * 输入流json转Java对象
+     * @param inputStream json输入流
+     * @param javaType 最终解析的Java对象类Type对应的JavaType
+     * @return
+     */
     public static Object inputStreamToObject(InputStream inputStream, JavaType javaType){
         try {
             return objectMapper.readValue(inputStream, javaType);
@@ -37,6 +50,13 @@ public class JacksonUtil {
         }
     }
 
+    /**
+     *
+     * @param json json字符串
+     * @param clazz 结果类
+     * @return json解析结果对象
+     * @param <T>
+     */
     public static <T> T stringToClass(String json, Class<T> clazz){
         try {
             return objectMapper.readValue(json, clazz);
@@ -45,6 +65,12 @@ public class JacksonUtil {
         }
     }
 
+    /**
+     * 对象转json字符串
+     * @param object 要转成json的对象
+     * @param javaType 对象的Type对应的JavaType
+     * @return
+     */
     public static String objectToJson(Object object, JavaType javaType){
 
         try {
@@ -55,6 +81,12 @@ public class JacksonUtil {
 
     }
 
+    /**
+     * 普通对象转输出流
+     * @param object 对象
+     * @param out 输出流
+     * @throws IOException
+     */
     public static void objectToPrintStream(Object object, OutputStream out) throws IOException {
         JsonGenerator generator = objectMapper.getFactory().createGenerator(out, JsonEncoding.UTF8);
         ObjectWriter writer = objectMapper.writer();
