fb4cf54b510171d5943bc3a4ff4c7068a3684f43
oycm <1164864987@qq.com>
2024-02-29 21:17:56 +0800

    自定义遍历方法

--- src/main/java/com/oycm/spring_mvc_starter/source/core/SpringCoreConcurrentReferenceHashMap.java
+++ src/main/java/com/oycm/spring_mvc_starter/source/core/SpringCoreConcurrentReferenceHashMap.java
@@ -1,7 +1,5 @@
 package com.oycm.spring_mvc_starter.source.core;
 
-import org.springframework.util.ConcurrentReferenceHashMap;
-
 import java.util.Iterator;
 
 /**
@@ -11,15 +9,30 @@ import java.util.Iterator;
 public class SpringCoreConcurrentReferenceHashMap {
 
     public static void main(String[] args) {
-        ConcurrentReferenceHashMap<String, String> referenceMap = new ConcurrentReferenceHashMap<>();
 
-        System.out.println(referenceMap.put("key", "value"));
+        for (long i = Long.MIN_VALUE; i <= Long.MAX_VALUE ; i++) {
+            StringBuilder s1 = new StringBuilder();
+            StringBuilder s2 = new StringBuilder();
 
-        System.out.println(referenceMap);
+            MyIterator iterator = new MyIterator(i);
+            while (iterator.hasNext()){
+                s1.append(iterator.next()).append(",");
+            }
+
+            EnumSetIterator enumSetIterator = new EnumSetIterator(i);
+            while (enumSetIterator.hasNext()){
+                s2.append(enumSetIterator.next()).append(",");
+            }
+            if (!s1.toString().equals(s2.toString())){
+                System.err.println(s1);
+                System.err.println(s2);
+            }
+            break;
+        }
 
     }
 }
-class MyIterator  implements Iterator<Long> {
+class MyIterator implements Iterator<Long> {
 
     long unseen;
 
@@ -35,12 +48,39 @@ class MyIterator  implements Iterator<Long> {
     }
 
     @Override
-    public java.lang.Long next() {
-
+    public Long next() {
+        if (unseen == 0){
+            throw new RuntimeException("no element");
+        }
+        // 从右边开始找1
         lastReturned = Long.numberOfTrailingZeros(unseen);
 
-        unseen = (unseen >> lastReturned);
+        unseen &= ~(1L << lastReturned);
 
         return lastReturned;
+    }
+}
+class EnumSetIterator implements Iterator<Integer> {
+
+    long unseen;
+
+    long lastReturned = 0;
+
+    EnumSetIterator(Long unseen) {
+        this.unseen = unseen;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return unseen != 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (unseen == 0)
+            throw new RuntimeException();
+        lastReturned = unseen & -unseen;
+        unseen -= lastReturned;
+        return Long.numberOfTrailingZeros(lastReturned);
     }
 }
