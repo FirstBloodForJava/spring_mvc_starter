f64f98edb0bb9c6f37edb80c41178e19a09786e4
ouyangcm <mingorg@163.com>
2024-03-04 13:22:39 +0800

    弱引用

---
+++ src/main/java/com/oycm/spring_mvc_starter/ref/WeakReferenceUtils.java
@@ -0,0 +1,39 @@
+package com.oycm.spring_mvc_starter.ref;
+
+import com.oycm.spring_mvc_starter.ref.pojo.BigObject;
+
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ouyangcm
+ * create 2024/3/4 10:36
+ */
+public class WeakReferenceUtils {
+    public static void main(String[] args) {
+
+        ReferenceQueue<BigObject> referenceQueue = new ReferenceQueue<BigObject>();
+        List<WeakReference<BigObject>> referenceList = new ArrayList<>();
+        List<BigObject> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            WeakReference<BigObject> userWeakReference = new WeakReference<>(new BigObject(), referenceQueue);
+            if (i < 10){
+                list.add(userWeakReference.get());
+            }
+            referenceList.add(userWeakReference);
+            Reference<? extends BigObject> temp = referenceQueue.poll();
+            System.out.println(i + ": " + temp);
+        }
+        for (int i = 0; i < referenceList.size(); i++) {
+            if (referenceList.get(i).get() != null){
+                System.out.println(i + " hava " + referenceList.get(i));
+            }else {
+                System.out.println(i + ": " + referenceList.get(i));
+            }
+        }
+        System.out.println();
+    }
+}
