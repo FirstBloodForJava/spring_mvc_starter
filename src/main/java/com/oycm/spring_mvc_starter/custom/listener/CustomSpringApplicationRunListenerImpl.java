90b7bb5240aae2353d6ead2fa7a90f8de1ab3f05
ouyangcm <mingorg@163.com>
2024-03-05 10:08:11 +0800

    SpringBoot的约定大于配置

---
+++ src/main/java/com/oycm/spring_mvc_starter/custom/listener/CustomSpringApplicationRunListenerImpl.java
@@ -0,0 +1,46 @@
+package com.oycm.spring_mvc_starter.custom.listener;
+
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author ouyangcm
+ * create 2024/3/5 10:02
+ */
+public class CustomSpringApplicationRunListenerImpl implements SpringApplicationRunListener {
+    @Override
+    public void starting() {
+
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+
+    }
+}
