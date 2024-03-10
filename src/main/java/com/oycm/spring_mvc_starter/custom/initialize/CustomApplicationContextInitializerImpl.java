48de0e5e46b84ebc13b5eaed1f6f5d52129ad6ee
oycm <1164864987@qq.com>
2024-03-10 21:57:01 +0800

    init实现

---
+++ src/main/java/com/oycm/spring_mvc_starter/custom/initialize/CustomApplicationContextInitializerImpl.java
@@ -0,0 +1,11 @@
+package com.oycm.spring_mvc_starter.custom.initialize;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+public class CustomApplicationContextInitializerImpl implements ApplicationContextInitializer<XmlWebApplicationContext> {
+    @Override
+    public void initialize(XmlWebApplicationContext applicationContext) {
+
+    }
+}
