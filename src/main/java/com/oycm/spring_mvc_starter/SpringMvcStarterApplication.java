96355540aec9714fcd928a93398d1ac585703c28
oycm <1164864987@qq.com>
2024-03-24 22:14:11 +0800

    MessageSource 消息配置

--- src/main/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplication.java
+++ src/main/java/com/oycm/spring_mvc_starter/SpringMvcStarterApplication.java
@@ -1,18 +1,24 @@
 package com.oycm.spring_mvc_starter;
 
 import com.oycm.spring_mvc_starter.config.PropertiesConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// (scanBasePackages = {"org"})
 @SpringBootApplication
 @RestController
 public class SpringMvcStarterApplication {
 
+    @Autowired
+    private MessageSource messageSource;
     @RequestMapping("/")
     public String home() {
+        String message = messageSource.getMessage("hello.message", new Object[]{"123"}, null);
+        System.out.println(message);
         return "Hello World!";
     }
 
